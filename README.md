7ad6eebb06e13a7d16ee91a1eea597a9a4969cb1
Jeff Blaisdell <blaisdje@yahoo.com>
2013-03-27 12:36:22 -0500

    Added project body.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node-Handlebars-Compiler
+### A simple file watcher that will compile .handlebars files on save.
---
+++ watch.js
@@ -0,0 +1,48 @@
+var argv  = require('optimist').argv,
+    watch = require('watch'),
+    exec  = require('child_process').exec,
+    props = {
+		'handlebars' : 'handlebars ${resource_loc} -f ${output_resource_path} -a true'
+    };
+
+var handlebars = function(f) {
+      console.log('Processing file: ' + f);
+
+      var output = f.replace(/\.handlebars/, '.js'),
+          cmd    = props.handlebars.replace(/\$\{resource_loc\}/, f),
+          cmd    = cmd.replace(/\$\{output_resource_path\}/, output),
+          child  = exec(cmd);
+
+      child.stderr.on('data', function(err) {
+      	console.log(['Error executing handlebars compile: ', err]);
+      });
+};
+
+var handlebarsFilter = function(f, stat) {
+
+	if(stat.isDirectory()) {
+		// Keep recursing.
+		return false;
+	} else if (f && f.match(/\.handlebars$/)) {
+		// Found.  Do not filter.
+		return false;
+	} else {
+		// Not a diretory or .handlbebars - filter it.
+		return true;
+	};
+};
+
+var dir = argv.watchdir
+console.log(['Watching directory: ', dir]);
+
+watch.createMonitor(dir, {'filter': handlebarsFilter},  function (monitor) {
+
+    monitor.on('created', handlebars);
+
+    monitor.on('changed', handlebars);
+
+    monitor.on('removed', function (f, stat) {
+      // Handle removed files
+    });
+
+ });
